2876035036ff78b563f54681d560fec4592466b2
Igor <lost.autumn93@gmail.com>
2020-11-10 22:05:27 +0600

    init all

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+
+function get_user_by_email($email){
+    $db = new PDO("mysql:host=localhost; dbname=registration", "root", "root");
+    $sql = "SELECT * FROM users WHERE email=:email";
+    $statement = $db->prepare($sql);
+    $statement->execute(['email' => $email]);
+    $user = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $user;
+};
+
+function add_user($email, $password) {
+    $db = new PDO("mysql:host=localhost; dbname=registration", "root", "root");
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $statement = $db->prepare($sql);
+    $statement->execute([
+        'email'    => $email,
+        'password' => password_hash($password, PASSWORD_DEFAULT)
+    ]);
+};
+
+function set_flash_message($name, $message) {
+    $_SESSION[$name] = $message;
+};
+
+function display_flash_message($name) {
+    if(isset($_SESSION[$name])) {
+        echo '<div class="alert alert-'. $name .' text-dark" role="alert">' . $_SESSION[$name] . '</div>';
+        unset($_SESSION[$name]);
+    }
+}
+
+function redirect_to($path){
+    header('Location: /' . $path);
+
+};
